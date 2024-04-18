5cf32c8ca7e76d5200530f5037b0f42e13aac719
marcelogdiaz <marcelogdiaz@gmail.com>
2024-04-18 17:20:57 -0300

    Ejemplos clase 5 y 6

--- C5_NUMEROS/Program.cs
+++ C5_NUMEROS/Program.cs
@@ -10,41 +10,62 @@ namespace C5_NUMEROS
     {
         static void Main(string[] args)
         {
-            Console.WriteLine($"int.MinValue = {int.MinValue}");
-            Console.WriteLine($"int.MaxValue = {int.MaxValue}");
-            Console.WriteLine($"float.MinValue = {float.MinValue}");
-            Console.WriteLine($"float.MaxValue = {float.MaxValue}");
-            Console.WriteLine($"double.MinValue = {double.MinValue}");
-            Console.WriteLine($"double.MaxValue = {double.MaxValue}");
-            Console.WriteLine($"decimal.MinValue = {decimal.MinValue}");
-            Console.WriteLine($"decimal.MaxValue = {decimal.MaxValue}");
-            Console.WriteLine($"short.MinValue = {short.MinValue}");
-            Console.WriteLine($"short.MaxValue = {short.MaxValue}");
-            Console.WriteLine($"long.MinValue = {long.MinValue}");
-            Console.WriteLine($"long.MaxValue = {long.MaxValue}");
+            //Console.WriteLine($"int.MinValue = {int.MinValue}");
+            //Console.WriteLine($"int.MaxValue = {int.MaxValue}");
+            //Console.WriteLine($"float.MinValue = {float.MinValue}");
+            //Console.WriteLine($"float.MaxValue = {float.MaxValue}");
+            //Console.WriteLine($"double.MinValue = {double.MinValue}");
+            //Console.WriteLine($"double.MaxValue = {double.MaxValue}");
+            //Console.WriteLine($"decimal.MinValue = {decimal.MinValue}");
+            //Console.WriteLine($"decimal.MaxValue = {decimal.MaxValue}");
+            //Console.WriteLine($"short.MinValue = {short.MinValue}");
+            //Console.WriteLine($"short.MaxValue = {short.MaxValue}");
+            //Console.WriteLine($"long.MinValue = {long.MinValue}");
+            //Console.WriteLine($"long.MaxValue = {long.MaxValue}");
 
-            double numdouble = 3.78;
-            decimal numdecimal = 3.78M;
-            int entero;
-            entero = (int)numdouble;
-            entero = (int)numdecimal;
-            Console.WriteLine(entero);
+            //double numdouble = 3.78;
+            //decimal numdecimal = 3.78M;
+            //int entero;
+            //entero = (int)numdouble;
+            //entero = (int)numdecimal;
+            //Console.WriteLine(entero);
 
             int i = 3;
             int j = i++;
 
 
             Console.WriteLine(i);
-                Console.WriteLine(j);
+            Console.WriteLine(j);
 
             int k = ++i;
 
-            //int l = 5;
-            ////l += 9;
+            int l = 5;
+            //l += 9;
 
-            //l = +9;
+            l = +9;
+            var valor = 16.8;
+            int a = 20 / 10 / 2;
+            int b = 20 / (10 / 2);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
 
+            if (a==b)
+            {
+                Console.WriteLine("A y B son iguales");
+            }
+            else
+            {
+                Console.WriteLine("A y B NO son iguales");
+            }
 
+            if (a != b)
+            {
+                Console.WriteLine("A y B son distintos");
+            }
+            else
+            {
+                Console.WriteLine("A y B NO son distintos");
+            }
             Console.ReadLine();
         }
     }
---
+++ C5_STRING/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C5_STRING
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string a = "hello";
+            string b = "h";
+            // Append to contents of 'b'
+            b += "ello";
+
+            //IGUALDAD
+            Console.WriteLine(a == b);
+            Console.WriteLine(object.ReferenceEquals(a, b));
+            Console.WriteLine(b);
+
+            string c = "algo";
+            b = c;
+            a = b;
+
+            Console.WriteLine("A apunta C");
+            Console.WriteLine(object.ReferenceEquals(a, c));
+
+
+            //CREACION DE STRING
+            //from string literal and string concatenation
+            //string fname, lname;
+            //fname = "Rowan";
+            //lname = "Atkinson";
+
+            //char[] letters = { 'H', 'e', 'l', 'l', 'o' };
+
+            //string fullname = (fname + lname);
+
+
+            //Console.WriteLine("Full Name:" + fullname);
+            //Console.WriteLine("Full Name: {0}", fullname);
+            //Console.WriteLine($"Full Name: {fullname}");
+            ////by using string constructor { 'H', 'e', 'l', 'l','o' };
+            //string greetings = new string(letters);
+
+
+            //string text = @"My pensive SARA ! thy soft cheek reclined
+            //            Thus on mine arm, most soothing sweet it is
+            //            To sit beside our Cot,...";
+
+            //Console.WriteLine(text);
+
+            string frase1 = "Her name was \"Sara.\"";
+            string frase2 = @"Her name was ""Sara.""";
+            //Console.WriteLine(frase1);
+
+            if (frase1 == frase2)
+            {
+                Console.WriteLine("Frases iguales");
+            }
+            else {
+                Console.WriteLine("Frases distintas");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
